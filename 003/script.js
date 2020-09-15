c6682e7d8b11d0de18bb1f330ec96c0f17b7aa00
On work <weh4teyou@gmail.com>
2020-09-15 14:06:20 +0300

    Первый комит

---
+++ 003/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var leftBorderWidth = 1;
+
+{
+let second = 2;
+
+console.log(second);
+}
+
+const pi = 3.14;
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]); 
+
+// alert("i want to go home");
+
+// let answer = confirm("Are you idiot?");
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?" , "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+
+// console.log(4 + + " - object");
+
+
+// 006)
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log( )
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log("Working")
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log("Working..")
+// }
+
+// 2)
+
+// console.log(typeof(Boolean('5')));

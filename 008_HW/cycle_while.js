9760baa52ebb176ac9d6500a7ddce45835cf536f
homeJS <weh4teyou@gmail.com>
2020-09-20 22:08:58 +0300

    Задание 3. С циклами

--- 008_HW/cycle_while.js
+++ 008_HW/cycle_while.js
@@ -14,7 +14,23 @@ let appData = {
 };
 
 
-    for (let i = 0; i < 2; i++) {
+    // for (let i = 0; i < 2; i++) {
+    //     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    //         b = prompt("Во сколько обойдется?", '');
+
+    //     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+    //         && a != '' && b != '' && a.length < 50) {
+    //             console.log('done');
+    //             appData.expenses[a] = b;
+    //     }else {
+
+    //     }
+
+        
+    // };
+
+     let i = 0;
+    while (i < 2) {
         let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
             b = prompt("Во сколько обойдется?", '');
 
@@ -22,12 +38,12 @@ let appData = {
             && a != '' && b != '' && a.length < 50) {
                 console.log('done');
                 appData.expenses[a] = b;
-        }else {
+        }else break; {
 
         }
 
-        
-    };
+        i++;
+    }
 
     appData.moneyPerDay = appData.budget / 30;
 
@@ -44,12 +60,3 @@ if (appData.moneyPerDay < 100) {
     console.log("Произошла ошибка");
 }
 
-
-// appData.expenses.obyaz = prompt("Введите обязательную статью расходов в этом месяце", "");
-
-// appData.expenses.neobyaz = prompt("Во сколько обойдется?", "");
-
-    // appData.expenses.a1 = a2;
-
-    // a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    // a4 = prompt("Во сколько обойдется?", "");
